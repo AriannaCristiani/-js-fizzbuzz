838bda7ff7db494052715c833c7cc586c735738c
AriannaCrisriani <ariannacristiani@yahoo.it>
2024-10-15 14:13:12 +0200

    creazione file più aggiunta commenti per il procedimento

---
+++ app.js
@@ -0,0 +1,5 @@
+//Procedimento:
+// Scrivivere un programma che stampi i numeri da 1 a 100,
+//   SE multipli di 3 stampare la stringa “Fizz” al posto del numero 
+//      ALTRIMENTI SE multipli di 5 stampare la stringa "Buzz". 
+//           ALTRIMENTI per i numeri che sono sia multipli di 3 che di 5 stampare la stringa "FizzBuzz".
